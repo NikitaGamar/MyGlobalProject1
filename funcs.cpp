160ab6bd0718fc0d847d7ee7bd8115495a970907
NikitaGamar <nikitagamar10@yandex.ru>
2017-11-22 22:24:16 +0300

    add function MyPow()

--- funcs.cpp
+++ funcs.cpp
@@ -6,7 +6,7 @@ double MySum(double a, double b)
 return a + b;
 }
 double MySub(double a, double b)
-{ 
+{
 return a - b;
 }
 double MyMul(double a, double b)
@@ -17,3 +17,11 @@ double MyDiv(double a, double b)
 {
 return a / b;
 }
+int MyPow(int num, int n)
+{
+int res = num;
+for (int i = 1; i < n; i++)
+{
+res *= num;
+}
+return res;
